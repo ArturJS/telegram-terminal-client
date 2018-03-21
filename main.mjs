a1c639edb6320a1265a6987f437a9b9c6c4f8aee
ArturN <nizamutdin.art@yandex.ru>
2018-03-21 06:16:46 +0400

    Fixed bug with empty content after page.goto(...)

--- main.mjs
+++ main.mjs
@@ -1,17 +1,13 @@
 import puppeteer from 'puppeteer';
 
-const sleep = async (delay) => {
-    return new Promise(resolve => {
-        setTimeout(resolve, delay);
-    });
-};
-
-(async()=>{
+(async() => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://web.telegram.org/#/login');
-    // todo: solve the problem when we need to wait manually for page load
-    await sleep(5000); 
+
+    // see also https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#pagegotourl-options
+    await page.goto('https://web.telegram.org/#/login', {
+        waitUntil : 'networkidle0'
+    });
     await page.screenshot({path: 'example-telegram-login.png'});
 
     await browser.close();
